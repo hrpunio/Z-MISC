00fa6571ce1f105898284d9419844c40fcb3a8ef
tomasz przechlewski <rudolf.von.ems@gmail.com>
2017-10-31 09:08:13 +0100

    dodanie przykładu zamki

---
+++ GeoPhotos/TeutonicCastles/setDescription.sh
@@ -0,0 +1,9 @@
+exiftool -Description="Wolfschanze" 14719109232_deff996c8f_o_r.jpg
+exiftool -Description="Kwidzyn Castle" 23432735994_4fd0c9db53_o_r.jpg
+exiftool -Description="Gutenberg Bible" 23765421340_14200e0a49_o_r.jpg
+exiftool -Description="Frombork Castle" 32429762032_de1148d439_o_r.jpg
+exiftool -Description="Sztum Castle" 3557531704_60491461e9_o_r.jpg
+exiftool -Description="Battlefield in Stembark" 35812875932_86bd3130bf_o_r.jpg
+exiftool -Description="Gniew Castle" 6207545536_3e0f7c250d_o_r.jpg
+exiftool -Description="Malbork Castle" 6539047845_a8c64973f3_o_r.jpg
+
